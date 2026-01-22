a14e483c09fd9abfa918859ebe7c633dffdfd156
ADRIEN BANGMA <adrien.bangma@atos.net>
2026-01-22 14:45:36 +0100

    generate api

--- backend/bdclimats/bdclimats/settings.py
+++ backend/bdclimats/bdclimats/settings.py
@@ -39,7 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'catalog',
+    'rest_framework',
+    'api',
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 20,
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
